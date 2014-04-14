5b9e3e2c950a2d10e8ecd89cab192cf1a72c323e
Sorrow <feniksvergil@gmail.com>
2014-04-14 20:05:38 +0300

    Seasonvar.ru serial parser. Parse to listbox

---
+++ SerialParcer/Backup/SerialParcer/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Net;
+using System.IO;
+using System.Threading;
+
+namespace SerialParcer
+{
+    public partial class Form1 : Form
+    {
+        ListBox adressList = new ListBox();
+        public Form1()
+        {
+            InitializeComponent();
+            Thread parsThread = new Thread(SerialList);
+            parsThread.Start();
+        }
+
+        public void SerialList()
+        {
+            string urlSV = "http://seasonvar.ru";
+            string htmlSV = string.Empty;
+            string patternSerialList = @"href=""(.+?)"" class=""betterT alf-link"">(.+?)</a>";
+            HttpWebRequest myRequestSV = (HttpWebRequest)HttpWebRequest.Create(urlSV);
+            HttpWebResponse myResponseSV = (HttpWebResponse)myRequestSV.GetResponse();
+            StreamReader srSV = new StreamReader(myResponseSV.GetResponseStream());
+            while (srSV.Peek() >= 0)
+            {
+                htmlSV = srSV.ReadLine();
+                Regex rgx1 = new Regex(patternSerialList);
+                MatchCollection resSV = rgx1.Matches(htmlSV);
+                if (resSV.Count > 0)
+                {
+                    foreach (Match match in resSV)
+                    {
+                        adressList.Items.Add(match.Groups[1].Value);
+                        listBox1.Items.Add(match.Groups[2].Value);
+                    }
+                }
+            }
+        }
+
+        public static string getBetween(string strSource, string strStart, string strEnd)
+        {
+            int Start, End;
+            if (strSource.Contains(strStart) && strSource.Contains(strEnd))
+            {
+                Start = strSource.IndexOf(strStart, 0) + strStart.Length;
+                End = strSource.IndexOf(strEnd, Start);
+                return strSource.Substring(Start, End - Start);
+            }
+            else
+            {
+                return "";
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int selectedIndex = listBox1.SelectedIndex;
+            string serialAdress = adressList.Items[selectedIndex].ToString();
+            string urln = "http://seasonvar.ru" + serialAdress;
+            linkLabel1.Text = urln;
+            string html = string.Empty;
+            string pattern = @"<img src=""(.+?)"" alt";
+            HttpWebRequest myRequest = (HttpWebRequest)HttpWebRequest.Create(urln);
+            HttpWebResponse myResponse = (HttpWebResponse)myRequest.GetResponse();
+            StreamReader sr = new StreamReader(myResponse.GetResponseStream());
+            html = sr.ReadToEnd();
+            string desc = getBetween(html, "<p>", "</p>");
+            richTextBox1.Text = desc;
+            string res;
+            res = Regex.Match(html, pattern).Groups[1].ToString();
+            
+            new WebClient().DownloadFile(res, "DescriptionPicture.jpg");
+            byte[] bmpData = new WebClient().DownloadData(res);
+            using (MemoryStream ms = new MemoryStream(bmpData))
+            {
+                Bitmap bmp = (Bitmap)Bitmap.FromStream(ms);
+                pictureBox1.Image = bmp;
+            }
+           
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string findPattern = textBox1.Text;
+            int ind = listBox1.FindString(findPattern);
+            if (ind > -1)
+                listBox1.SelectedIndex = ind;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(linkLabel1.Text);
+        }
+    }
+}
---
+++ SerialParcer/SerialParcer/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace SerialParcer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 35);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(187, 381);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(420, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(413, 277);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(206, 295);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(282, 121);
+            this.listBox2.TabIndex = 2;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(495, 295);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(338, 76);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(187, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(206, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(208, 277);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(495, 378);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(0, 13);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(495, 377);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(52, 37);
+            this.dataGridView1.TabIndex = 7;
+            this.dataGridView1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(842, 425);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Serial Parcer";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.ListBox listBox1;
+        public System.Windows.Forms.RichTextBox richTextBox1;
+        public System.Windows.Forms.ListBox listBox2;
+        public System.Windows.Forms.RichTextBox richTextBox2;
+        public System.Windows.Forms.TextBox textBox1;
+        public System.Windows.Forms.PictureBox pictureBox1;
+        public System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
